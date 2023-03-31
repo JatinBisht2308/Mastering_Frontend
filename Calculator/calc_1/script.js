aa34b81c181fa17dc72984e769c5a719864122f8
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-04-01 00:04:36 +0530

    :fire: created the logic for calculation clearing is left

---
+++ Calculator/calc_1/script.js
@@ -0,0 +1,18 @@
+let inputString ='';
+//buttons is an array
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '=')
+        {
+            inputString = eval(inputString);
+            document.querySelector('input').value = inputString;
+        }
+        else{
+        // console.log(e.target);
+        inputString+= e.target.innerHTML;
+        // console.log(inputString);
+        document.querySelector('input').value = inputString;
+        }
+    })
+});
