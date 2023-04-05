ccc4568fd8390cbdf1cfbcd1fe89664c085c4cca
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-04-05 15:41:02 +0530

    :fire: completed the logic for converting new zealand dollars to inr

--- Calculator/calc_1/script.js
+++ Calculator/calc_1/script.js
@@ -4,19 +4,19 @@ let buttons = document.querySelectorAll(".button");
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
-        if(inputString.includes('%'))
-        {
-            // iafp -> input array for percentage
-          const iafp = inputString.split("%");
-          console.log(iafp);
-          inputString = (iafp[0]*iafp[1])/100;
-          document.querySelector("input").value = inputString;
-        }
-        else if(inputString.includes('$'))
-        {
-            // we will convert the dollar(new zealand) amount to the inr amount 
-
-        }
+      if (inputString.includes("%")) {
+        // iafp -> input array for percentage
+        const iafp = inputString.split("%");
+        console.log(iafp);
+        inputString = (iafp[0] * iafp[1]) / 100;
+        document.querySelector("input").value = inputString;
+      } else if (inputString.includes("$")) {
+        // we will convert the dollar(new zealand) amount to the inr amount
+        let dArray = inputString.split("$");
+        console.log(dArray);
+        let inrAmount = dArray[0] * 51;
+        document.querySelector("input").value = inrAmount;
+      }
       inputString = eval(inputString);
       document.querySelector("input").value = inputString;
     } else if (e.target.innerHTML == "C") {
