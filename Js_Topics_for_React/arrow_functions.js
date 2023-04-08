032f62443060795c71d6bdd9e08060acfccdafa1
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-04-08 14:19:05 +0530

    :fire: arrow function over normal function

---
+++ Js_Topics_for_React/arrow_functions.js
@@ -0,0 +1,11 @@
+// this is a normal function
+function DoSomething()
+{
+    console.log("DoSomething");
+}
+
+// this is an arrow function
+const DoSomethingElse = () =>{
+    console.log("DoSomething Else");
+}
+DoSomethingElse();
