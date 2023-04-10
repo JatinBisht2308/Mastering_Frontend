16f4e2528831c890fcba98f97334f81d9c7ebaab
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-04-10 22:22:32 +0530

    :fire: done with teranry opertor over if else

--- Js_Topics_for_React/arrow_functions.js
+++ Js_Topics_for_React/arrow_functions.js
@@ -9,3 +9,10 @@ const DoSomethingElse = () =>{
     console.log("DoSomething Else");
 }
 DoSomethingElse();
+
+// anoynamous function below code is for react(the function which has no name in inside the onClick function is an anoynamous function which will be automatically called when the button is clicked.)
+<button onClick={() =>{
+    console.log("clicked");
+}}>
+    click ME
+</button>
---
+++ Js_Topics_for_React/ternary_operators.js
@@ -0,0 +1,15 @@
+// here we will use ternary operators over if else statement in js
+let age = 11;
+let name = "unknown";
+if(age > 10){
+ name = "Jatin";
+}
+else{
+  name = "Pedro";
+}
+console.log(name);
+
+// using ternary operator
+let newAge = 5;
+let newName = newAge > 10 ? "Jatin" : "Pedro";
+console.log(newName);
