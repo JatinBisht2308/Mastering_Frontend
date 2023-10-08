8aa19bba21ccaefb327af9b318d5f8ea1d32f9fa
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-10-08 21:33:49 +0530

    :fire: created a custom app inside the main.jsx to understand how react uses js to convert the coding syntax

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function customApp(){
+  return (
+    <div>
+      <h1>Hi from custom app!</h1>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <App />
+    customApp()
 )
