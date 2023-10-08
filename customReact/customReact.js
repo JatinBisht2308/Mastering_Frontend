d23e098284d970eaf57946a2136017b43ccf405c
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-10-08 21:29:14 +0530

    :fire: completed the customRender function using the for in loops

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,10 +1,19 @@
 function customRender(reactElement,container)
 {
+    // const domElement = document.createElement(reactElement.type);
+    // domElement.innerHTML = reactElement.children;
+    // domElement.setAttribute('href',reactElement.props.href);
+    // domElement.setAttribute('target',reactElement.props.target);
+    // container.appendChild(domElement);
+    
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children;
-    domElement.setAttribute('href',reactElement.props.href);
-    domElement.setAttribute('target',reactElement.props.target);
-    container.appendChild(domElement);
+    for(const prop in reactElement.props){
+        // prop is the key
+        // reacElement.props[prop] is the value
+       domElement.setAttribute(prop,reactElement.props[prop]);
+    }
+      container.appendChild(domElement)
 }
 
 const reactElement = {
