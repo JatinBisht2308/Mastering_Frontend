42481f41644ad04631ea299aa341c812c0c51767
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-10-08 15:23:48 +0530

    :fire: added another project using vite and will soon add more learning chai or react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to chai or react + vite</h1>
+    </>
+  )
+}
+
+export default App
--- Firebase_Learning/script.js
+++ Firebase_Learning/script.js
@@ -2,5 +2,6 @@ const inputDataElement = document.getElementById('item');
 const addButtonElement = document.getElementById('add');
 // make and eventlistener
 addButtonElement.addEventListener('click',function(){
-    console.log(inputDataElement.value);
+    let item = inputDataElement.value;
+    console.log(item);
 });
