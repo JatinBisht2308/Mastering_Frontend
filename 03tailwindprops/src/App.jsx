3eb89f0f50afb18ceea3e8f130c950dc945b045b
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-10-09 21:57:51 +0530

    :fire: one way to use props in other components

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
-import Card from './components/Card';
+import "./App.css";
+import Card from "./components/Card";
 function App() {
-
+  const myArr = [1,2,3,4];
+  const myObj = {
+    college: "DIT University",
+    rollNo: "200102567"
+  }
   return (
     <>
-      <h1 className='bg-green'>Hi using Tailwind css!</h1>
-      <Card/>
+      <h1 className=" bg-sky-500 text-white rounded-lg px-2 py-2 mb-2">
+        Tailwind css
+      </h1>
+      {/* you cannot pass array from here you have to declare it as a variable then you can pass it in props either array or object */}
+      {/* <Card name="Jatin" myArr=[1,2,3,4] /> myObj={college:"DIT University"} */}
+      {/* rather than it you can set the array as variable then pass it here in the props */}
+      <Card name="Riya" myArr={myArr} myObj={myObj} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
