badcd39e8d5ee4895943ad6b958b145a49a4ffde
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-10-09 21:42:50 +0530

    :fire: day 03 react learning props, tailwind.css

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from './components/Card';
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green'>Hi using Tailwind css!</h1>
+      <Card/>
+    </>
+  )
+}
+
+export default App
