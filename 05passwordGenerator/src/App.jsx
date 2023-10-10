ae751d21660156d5730f5f66d4a8745c9e2cc8e4
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-10-10 22:42:22 +0530

    :fire: added another state for the password

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -5,12 +5,13 @@ function App() {
   const [length, setLength] = useState(8);
   const [numbersPresent, setNumberPresent] = useState(false);
   const [characterPresent, setCharacterPresent] = useState(false);
+  const [password,setPassword] = useState("");
   return (
     <div className="w-full h-screen flex content-center items-center">
       <div className="fixed flex flex-wrap flex-col content-center text-white backdrop-blur-sm bg-white/20 px-10 p-10 rounded-xl">
         <div className="flex text-black w-full">
-          <input type="text text-black" className=" p-2 rounded-l-md w-full" />
-          <button className="bg-blue-400 p-2 rounded-r-md text-white">
+          <input type="text" className=" p-2 rounded-l-md w-full text-orange-400 text-md border-none" />
+          <button className="bg-blue-500 p-2 rounded-r-md text-white">
             copy
           </button>
         </div>
