abc5848a312b8b4cff6b030cbac4a21e6a1a8b2a
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-10-10 22:38:24 +0530

    :fire: added the range slider in the second div

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -6,18 +6,25 @@ function App() {
   const [numbersPresent, setNumberPresent] = useState(false);
   const [characterPresent, setCharacterPresent] = useState(false);
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen flex content-center items-center">
       <div className="fixed flex flex-wrap flex-col content-center text-white backdrop-blur-sm bg-white/20 px-10 p-10 rounded-xl">
-        <div className="flex text-black">
-          <input type="text text-black" className=" p-2 rounded-l-md" />
-          <button className="bg-blue-400 p-2 rounded-r-md text-white">copy</button>
+        <div className="flex text-black w-full">
+          <input type="text text-black" className=" p-2 rounded-l-md w-full" />
+          <button className="bg-blue-400 p-2 rounded-r-md text-white">
+            copy
+          </button>
         </div>
         <div className="flex ">
-          <p className="mr-3">Length {length}</p>
-          <input type="checkbox" name="number" id="num" className="mr-1"/>
-          <label htmlFor="num" className="mr-2">Numbers</label>
+          <input type="range" name="lengthRange" id="lr" className="mr-2" />
+          <p className="mr-4">Length {length}</p>
+          <input type="checkbox" name="number" id="num" className="mr-1" />
+          <label htmlFor="num" className="mr-3">
+            Numbers
+          </label>
           <input type="checkbox" name="character" id="char" className="mr-1" />
-          <label htmlFor="num" className="mr-2">Characters</label>
+          <label htmlFor="num" className="mr-1">
+            Characters
+          </label>
         </div>
       </div>
     </div>
